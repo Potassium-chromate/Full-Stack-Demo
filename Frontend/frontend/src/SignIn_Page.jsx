fb1484f9ecf7bf6e402d7cde2ac194d0e34c8565
Potassium-chromate <home7438072@gmail.com>
2025-07-01 22:33:47 +0800

    Feat: Implement login and signup functionality
    - Added user signup and login pages with form validation
    - Integrated backend API for user authentication
    - Store login status in a cookie for session persistence
    - Enhanced UI with improved CSS styling for a better user experience

---
+++ Frontend/frontend/src/SignIn_Page.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import axios from "axios";
+import "./CSS/SignIn_page.css"
+
+function SignIn_Page() {
+  const [identiftInfo, setIdentiftInfo] = useState({"account": "", "password": ""});
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+
+  const handleInputChange =(e) => {
+    const {name, value} = e.target
+    setIdentiftInfo({
+      ...identiftInfo,
+      [name]: value
+    })
+  }
+
+  const handleLogin =() => {
+    console.info(identiftInfo)
+     axios.post("http://127.0.0.1:8080/login", {
+            username: identiftInfo.account,
+            password: identiftInfo.password
+        })
+        .then(response => {
+            console.log("Log in successfully");
+            alert("Log in successfully");
+            return true;
+        })
+        .catch(error => {
+            console.error("API request error: ", error.response.data);
+            alert(error.response.data);
+            return false;
+	    }
+        );
+  }
+
+  const handleSignUp =() => {
+    console.info(identiftInfo)
+     axios.post("http://127.0.0.1:8080/signup", {
+            username: identiftInfo.account,
+            password: identiftInfo.password,
+            role: "USER"
+        })
+        .then(response => {
+            console.log("Sign up successfully");
+            alert("Sign up successfully");
+            return true;
+        })
+        .catch(error => {
+            console.error("API request error: ", error.response.data);
+            alert(error.response.data);
+            return false;
+        });
+  }
+
+  return (
+    <>
+      <div className='signin_page'>
+        <div className='signin_content'>
+            <h1>User Login</h1>
+            <label>Account</label>
+            <input type='text' name="account" onChange={handleInputChange}/>
+            <label>Passwor</label>
+            <input type='password' name='password' onChange={handleInputChange}/>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleSignUp}>Sign up</button>
+        </div>
+      </div>
+    </>  
+  )
+}
+
+export default SignIn_Page
